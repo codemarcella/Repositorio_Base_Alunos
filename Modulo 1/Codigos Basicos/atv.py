cb0a02e0c1a296c90f45d72f7db621bad5fe8385
codemarcella <marcellacastro.1301@gmail.com>
2025-09-30 11:10:53 -0300

    Add files via upload

---
+++ Modulo 1/Codigos Basicos/atv.py
@@ -0,0 +1,7 @@
+nome=input("qual seu nome?")
+idade=int(input("Qual sua idade?"))
+
+if idade>= 18:
+    print("Maior de idade")
+else:
+    print("Menor de idade")
---
+++ Modulo 1/Codigos Basicos/atvidade.py
@@ -0,0 +1,6 @@
+Lista=(31,43,56,78)
+#para cada item dentro da lista faça isso
+numero=20
+for item in Lista:
+    print(item-numero)
+    
